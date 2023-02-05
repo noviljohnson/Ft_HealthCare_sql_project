841cba55ef74b2cb65b21db1c21101df10944ae8
Novil Johnson <noviljohnson9@gmail.com>
2023-02-05 13:46:46 +0530

    extra fldr deleted

--- Problem_statements/prblm_stmnts_2.sql
+++
@@ -1,8 +0,0 @@
--- Active: 1671700995648@@127.0.0.1@3308@healthcare
-
-
--- Problem Statement 1: A company needs to set up 3 new pharmacies, they have come up with an
--- idea that the pharmacy can be set up in cities where the pharmacy-to-prescription ratio is the lowest
--- and the number of prescriptions should exceed 100. Assist the company to identify those cities
--- where the pharmacy can be set up.
-
